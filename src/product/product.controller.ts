a7d594344a78b91cbb36415099b0f0fe8ae9de58
vsb <vsb@cin.ufpe.br>
2022-07-02 18:57:17 -0300

    create getProduct function

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { ProductService } from './product.service';
-
+import { Request, Response } from 'express'; 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  async getProduct(
+    @Req() request: Request,
+    @Res() response: Response,
+  ) {
+    const { id } = request.query;
+
+    if(!id) {
+      response.status(400).send({ msg: 'missing id' });
+    }
+
+    response.status(200).send({ name: 'notebook', price: '5$' });
+  }
 }
