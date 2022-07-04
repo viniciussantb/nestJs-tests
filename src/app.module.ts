09b336b2a3392c310cc091f2063a01da6984c8de
vsb <vsb@cin.ufpe.br>
2022-07-04 11:50:37 -0300

    <feat> database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [],
+      synchronize: process.env.TYPEORM_SYNCHRONIZE,
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
