738b6d1228fa9c6cc41540232c55c1490e580222
vsb <vsb@cin.ufpe.br>
2022-07-06 23:38:13 -0300

    user controller unit test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,20 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Response } from 'express';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let spyService: UserService;
 
   beforeEach(async () => {
+    const ApiServiceProvider = {
+      provide: UserService,
+      useFactory: () => ({
+        create: jest.fn(() => []),
+      })
+    }
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [UserService, ApiServiceProvider],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
+    spyService = module.get<UserService>(UserService);
+
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
   });
+
+  describe('createUser', () => {
+    it('should create a user', () => {
+      const dto = {
+        name: 'josé',
+        username: 'jose12',
+        password: 'password',
+      }
+  
+      userController.create(dto);
+      expect(spyService.create).toHaveBeenCalled();
+      expect(spyService.create).toHaveBeenCalledWith(dto);
+    });
+  })
+
 });
