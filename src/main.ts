e9652021810e9e3b72660207ca97efc6f0776ec2
vsb <vsb@cin.ufpe.br>
2022-08-31 18:36:04 -0300

    <feat> initialize `AppDataSource`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppDataSource } from './data-source';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  await AppDataSource.initialize()
+  .then(() => console.log('bd up'))
+  .catch((error) => console.log(error));
+
+
   await app.listen(3000);
 }
 bootstrap();
